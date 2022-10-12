5bc8ee9b0cb08f6870115b79685d25d9569103f7
Rodrigo Valle <fernandovalle939@gmail.com>
2022-10-12 15:46:13 -0600

    Creando estilos globales

---
+++ src/navigation/GlobalStyle.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+    titulo:{
+      fontSize:16,
+      fontWeight:'bold',
+     
+    },
+    contenedor:{
+        flex:1,
+        marginTop:20,
+        marginHorizontal:'3.5%',
+        flexDirection:'column',
+        
+    },
+    Imgs:{
+        width:60,
+        height:60,
+        borderRadius:0,
+        
+    },
+    Subcontainer:{
+        marginVertical:6,
+        flex:1,
+        flexDirection:'row'
+    },
+    ImageContainer:{
+        flex:1
+    },
+    TextContaner:{
+        flex:4,
+        marginLeft:30
+    }
+});
+
+export default globalStyles
